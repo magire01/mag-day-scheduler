ef8bab082fc1ab171cf62f4d9516781cd0321e6d
Mark Gire <magire01@gmail.com>
2020-07-05 06:07:26 -0400

    Save buttons print test to console

--- script.js
+++ script.js
@@ -3,26 +3,55 @@ $(document).ready(function() {
     // Generate Hours
     var hourColumn = $("#row-event");
 
-
     function rowEvent() {
         for(var i = 8; i <= 18; i++) {
             console.log(i);
             
             var hourColumnCard = $("<div>");
             hourColumnCard.addClass("col-md-2", "card");
+            hourColumnCard.attr("id", "hour-card")
             hourColumnCard.text(i + ":00");
             hourColumn.append(hourColumnCard);
 
-            var saveColumnInputCard = $("<input>");
-            saveColumnInputCard.addClass("col-md-8");
-            hourColumn.append(saveColumnInputCard);
+            var saveColumnInput = $("<input>");
+            saveColumnInput.addClass("col-md-8");
+            saveColumnInput.attr("id", "event-input" + i)
+            hourColumn.append(saveColumnInput);
 
             var saveColumnButton = $("<button>");
             saveColumnButton.addClass("col-md-2");
+            saveColumnButton.attr("id", "save-button" + i);
+            saveColumnButton.attr("value", i);
             saveColumnButton.text("Save");
             hourColumn.append(saveColumnButton);
+            
+            // var saveButton = $("#save-button8");
+            hourColumn.delegate("#save-button" + i, "click", function() {
+            console.log("test");
+            
+            function saveButtonClick() {
+                hourColumn.delegate("#save-button" + i, "click", function() {
+                console.log("test");
+                })
+            }
+            
+            saveButtonClick();
+            })
         }
-    };
+        
+    }
+
+    function saveButtonClick() {
+        hourColumn.delegate("#save-button" + i, "click", function() {
+        console.log("test");
+        })
+    }
+
 
     rowEvent();
+    
+
+    
+
+    
 });
